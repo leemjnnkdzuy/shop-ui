7355f1f8376c4f62d9ee92ec3394565caa0c728d
LeeMjnnkDzuy <duylelv17@gmail.com>
2024-12-01 04:37:36 +0700

    Update API base URL in request utility

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ const check = 1;
 if (check === 2) {
     URL = "http://localhost:3001/";
 } else {
-    URL = "https://server-six-bay.vercel.app/";
+    URL = "https://pixelshop-api.leemjnnkdzuy.live/";
 }
 
 const request = axios.create({
