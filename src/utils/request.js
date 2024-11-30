13c72fa62072db08cb93cbfbc9c485200bf563e6
LeeMjnnkDzuy <duylelv17@gmail.com>
2024-12-01 02:51:33 +0700

    Remove unnecessary headers and request interceptor from axios instance

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,29 +5,7 @@ const URL =
 
 const request = axios.create({
 	baseURL: URL,
-	headers: {
-		'Content-Type': 'application/json',
-		'Accept': 'application/json',
-		'Access-Control-Allow-Origin': '*',
-		'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-	},
-	withCredentials: true
 });
-
-// Add request interceptor
-request.interceptors.request.use(
-	config => {
-		// Get token from storage
-		const token = localStorage.getItem('userToken') || sessionStorage.getItem('userToken');
-		if (token) {
-			config.headers.Authorization = `Bearer ${token}`;
-		}
-		return config;
-	},
-	error => {
-		return Promise.reject(error);
-	}
-);
 
 export default request;
 export { URL };
