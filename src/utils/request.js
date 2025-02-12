9ee76edaabd7073c9261de8696a9460e47f62b93
LeeMjnnkDzuy <duylelv17@gmail.com>
2025-02-12 23:59:29 +0700

    Update request URL to new live API endpoint

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,29 +1,27 @@
 import axios from "axios";
 
-let URL = "";
-
-URL = "https://pixelshop-api-cf28d2825764.herokuapp.com/";
+var URL = "https://pixelshop-api.leemjnnkdzuy.live/";
 
 const request = axios.create({
-    baseURL: URL,
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    validateStatus: status => {
-        return status >= 200 && status < 400;
-    }
+	baseURL: URL,
+	withCredentials: true,
+	headers: {
+		"Content-Type": "application/json",
+		Accept: "application/json",
+	},
+	validateStatus: (status) => {
+		return status >= 200 && status < 400;
+	},
 });
 
 request.interceptors.response.use(
-    response => response,
-    error => {
-        if (error.response && error.response.status === 401) {
-            console.log('Unauthorized access');
-        }
-        return Promise.reject(error);
-    }
+	(response) => response,
+	(error) => {
+		if (error.response && error.response.status === 401) {
+			console.log("Unauthorized access");
+		}
+		return Promise.reject(error);
+	}
 );
 
 export default request;
